e9614e0b0c49a5e44344d7f0c0122a74dfd644dd
Sean Martin <sean@seanmartinstudio.com>
2023-05-12 12:14:10 -0700

    adds Top Right Corner Line

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let pattern
-let sqX = 350
-let sqY = 350
+let sq1X = 250
+let sq1Y = 250
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -19,23 +19,31 @@ function draw() {
   rect(0, 0, windowWidth, windowHeight)
 
   //Static Square
-  rect(200, 200, 200)
+  rect(300, 200, 200)
   
-  //Top Left Corener
-  line(200, 200, sqX, sqY)
+  //Top Left Corner
+  line(300, 200, sq1X, sq1Y)
+
+  //Top Right Corner
+  line(500, 200, sq1X + 200, sq1Y)
 
   //Boston Left Corner
-  line(200, 400, sqX, sqY + 200)
+  line(300, 400, sq1X, sq1Y + 200)
 
   //Bottom Right Corner
-  line(400, 400, sqX + 200, sqY + 200)
+  line(500, 400, sq1X + 200, sq1Y + 200)
 
   //Moving Square
-  rect(sqX, sqY, 200)
+  rect(sq1X, sq1Y, 200)
 }
 
 function mouseDragged() {
-  sqX = mouseX
-  sqY = mouseY
+  sq1X = mouseX
+  sq1Y = mouseY
 }
+
+// function mouseDragged() {
+//   sq2X = mouseX
+//   sq2Y = mouseY
+// }
 
