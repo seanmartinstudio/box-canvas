eded1a3a7d6a11dafd9172e063979311211ccdd1
Sean Martin <sean@seanmartinstudio.com>
2023-05-18 12:50:17 -0700

    adds curser text and cleans up old code

--- sketch.js
+++ sketch.js
@@ -1,12 +1,20 @@
-// let myFont
+let myFont
 let pattern
 let sqX = 250
 let sqY = 225
+let isMouseClicked = false
 
-let animatedText = "PRESS AND HOLD MOUSE TO EXPAND BOX"
+let animatedText = "PRESS AND MOVE MOUSE TO EXPAND BOX"
+let titleText = "BOX CANVAS 1.0"
+let cursorText = "PRESS AND MOVE MOUSE TO EXPAND BOX"
+let buttonText = "CLICK TO SAVE CANVAS"
 let animatedTextX = 0
 let animatedTextY = 300 
 let speed = 2
+
+function preload() {
+  myFont = loadFont('/Roboto-Medium.ttf');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -19,39 +27,39 @@ function setup() {
   pattern = createPattern(buffer);
 
   //Creates Save Button
-  textFont('Helvetica');
-  button = createButton('CLICK TO SAVE IMAGE');
+  button = createButton(buttonText);
   button.position(0, 0);
   button.mousePressed(buttonClick);
-  button.position(windowWidth * .85, windowHeight * .05)
+  button.position(windowWidth * .85, windowHeight * .065)
+  textFont(myFont)
   button.style('background-color', color(255));
   button.style('border-radius', '4px');
-  // button.style("font-style", myFont)
 }
 
 function draw() {
   background(255)
   cursor(HAND)
  
-  //Draws Patter
+  //Draws Pattern
   fillPattern(pattern)
 
   //Static Square
   rect(300, 200, 200)
 
-  //Animates Text
-  animatedTextX += speed;
-  
-  //Display the text at the current position
-  textSize(32);
-  textAlign(CENTER);
-  text(animatedText, animatedTextX, animatedTextY);
+  //Builds text title
+  title()
 
   //Build top and bottom shapes
   createTopAndBottomShapes()
 
   //Moving Square
   rect(sqX, sqY, 200)
+
+  //Display cursor text
+  if (!isMouseClicked) {
+    textSize(11);
+    text(cursorText, mouseX + 10, mouseY - 10);
+  }
 }
 
 function mouseDragged() {
@@ -60,11 +68,11 @@ function mouseDragged() {
 }
 
 function buttonClick() {
-  save("myCustomBox.png");
+  save("BoxCanvasDrawing.png");
 }
 
 function createTopAndBottomShapes() {
-  //Bottom Of Box
+  //Bottom of Box
   beginShape();
   vertex(300, 400);
   vertex(500, 400);
@@ -72,7 +80,7 @@ function createTopAndBottomShapes() {
   vertex(sqX, sqY + 200);
   endShape(CLOSE);
 
-  //Top Of Box
+  //Top of Box
   beginShape();
   vertex(300, 200)
   vertex(500, 200)
@@ -81,11 +89,22 @@ function createTopAndBottomShapes() {
   endShape(CLOSE);
 }
 
-// function preload() {
-//   myFont = loadFont('/Roboto-Medium.ttf');
-// }
+//Created titleText
+function title() {
+  textFont(myFont)
+  textSize(32)
+  textAlign(CENTER)
+  text(titleText, windowWidth * .12, windowHeight * .10)
+}
 
-//Creates line guides for top and bottom shapes
+//Created cursorText
+function mousePressed() {
+  if(isMouseClicked === false) {
+    isMouseClicked = true
+  } 
+}
+
+//Creates line guides for top and bottom shapes (Not currently envoked in Draw)
 function createLineGuides() {
   //Top Left Corner
   line(300, 200, sqX, sqY)
@@ -96,6 +115,8 @@ function createLineGuides() {
   //Bottom Right Corner
   line(500, 400, sqX + 200, sqY + 200)
 }
+
+
 
 
 
